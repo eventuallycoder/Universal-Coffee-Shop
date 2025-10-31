d0da9994db6a0c259e8e5b395058a68df167ac00
Aaron Rosenberg <ajrosenberg20@gmail.com>
2025-10-31 18:05:03 -0500

    added some changes i guess

---
+++ app/models/user.py
@@ -0,0 +1,15 @@
+from models.authProvider import AuthProvider
+
+
+class User(AuthProvider):
+    def __init__(self, username, database):
+        self.username = username
+        self.database = database
+
+    def callback(self):
+        pass
+
+    def addAuthProvider(token):
+        pass
+
+    
---
+++ universal-coffee-shop/app/index.js
@@ -0,0 +1,8 @@
+//entry point of app, redirect to launch screen - test navigation here
+// universal-coffee-shop/app/index.js
+import { Redirect } from 'expo-router';
+
+export default function StartPage() {
+
+  return (<Redirect href="/launch" />);
+}
